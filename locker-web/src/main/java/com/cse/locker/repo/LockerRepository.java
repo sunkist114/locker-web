ea3c65991f2f9ca278e72186159f330ffaa12193
sunkist114 <minsun0114@naver.com>
2026-01-13 22:12:40 +0900

    chore: remove duplicated inner locker-web directory

--- locker-web/src/main/java/com/cse/locker/repo/LockerRepository.java
+++
@@ -1,6 +0,0 @@
-package com.cse.locker.repo;
-
-import com.cse.locker.domain.Locker;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LockerRepository extends JpaRepository<Locker, Integer> {}
